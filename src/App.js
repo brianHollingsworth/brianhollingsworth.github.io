5cb3f1922042d7adbcd1983ad52261961d6061fe
Brian Hollingsworth <bhollin42@gmail.com>
2019-06-29 21:42:42 -0500

    Temporarily creating an "under construction" page.

--- src/App.js
+++ src/App.js
@@ -8,16 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+          <code>I am currently refactoring brian.hollingsworth.codes into a <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          rel="noopener noreferrer">React</a> app.</code>
+          </p>
       </header>
     </div>
   );
