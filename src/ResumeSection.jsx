ea810c307c6586083a76c7f97a4dc979cf6cb82f
Brian Hollingsworth <brianhollingsworth@Brians-MBP.roc.mn.charter.com>
2019-12-03 21:24:23 -0600

    Created reusable ResumeSection component.

---
+++ src/ResumeSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function ResumeSection(props) {
+    return (
+        <NavLink to={props.route} exact style={{color: 'white', textDecoration: 'none'}} activeStyle={{color: '#61dafb', textDecoration: 'none'}}><FontAwesomeIcon icon={props.icon} /> {props.text}</NavLink>
+    );
+}
