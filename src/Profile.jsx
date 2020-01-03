ed99b5303fa6a00ea16ba23b9f16b7b021544957
Brian Hollingsworth <brianhollingsworth@Brians-MBP.roc.mn.charter.com>
2020-01-02 18:45:12 -0600

    Added Node.js resume repo link to profile.

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -8,7 +8,13 @@ export default function Profile(props) {
                 brian.hollingsworth.codes for a multitude of reasons: I wanted an always-changing
                 side project for experimenting with new web frameworks; updating a paper resume felt
                 antiquated, and LinkedIn became tiresome. Nothing makes me feel more homogeneous
-                than flaunting my profile on the same platform as every other developer out there.</p>
+                than flaunting my profile on the same platform as every other developer out there.
+                <br/>I am still in the process of fully migrating from my previous <a
+                className="App-link"
+                href="https://github.com/brianHollingsworth/brian.hollingsworth.codes"
+                target="_blank"
+                rel="noopener noreferrer">Node.js resume</a>, so feel free to pull from that repository 
+                in the meantime.</p>
         </div>
     );
 }
