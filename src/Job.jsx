066f8495ce8c0bde4759aecd9070002c866e8f95
Brian Hollingsworth <brianhollingsworth@Brians-MBP.roc.mn.charter.com>
2020-01-27 23:08:42 -0600

    Added tenure to job header and spacer to footer.

--- src/Job.jsx
+++ src/Job.jsx
@@ -5,8 +5,9 @@ const Job = props => {
     return (
         <div className="job">
             <div className="job-header">
-                <h4>{props.title}</h4>
-                <h6>{props.company}</h6>
+                <h4 className="job-title">{props.title}</h4>
+                <h5 className="job-company">{props.company}</h5>
+                <h5 className="job-tenure">{props.tenure}</h5>
             </div>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur odit, dolores unde atque recusandae expedita dignissimos eum tempora veniam perspiciatis.
--- src/WorkExperience.jsx
+++ src/WorkExperience.jsx
@@ -7,15 +7,16 @@ export default function WorkExperience(props) {
             <h2>Work Experience</h2>
             {/* TODO: Implement a read-from-JSON feature, similar to the
             original brian.hollingsworth.codes read displayed information. */}
-            <Job title="Software Engineer" company="Optum"/>
-            <Job title="Associate Software Engineer" company="Optum"/>
-            <Job title="Applications Developer II" company="Optum"/>
-            <Job title="Tech Dev Program Senior Associate" company="Optum"/>
-            <Job title="Tech Dev Program Intern" company="Optum"/>
-            <Job title="Software Engineering Intern" company="Maverick Software Consulting"/>
-            <Job title="Software Quality Assurance Intern" company="Symantec"/>
-            <Job title="Manager" company="Marnanteli's Pizza"/>
-            <Job title="Fulfillment Associate" company="Merrill Corporation"/>
+            <Job title="Software Engineer" company="Optum" tenure="October 2018 - Present"/>
+            <Job title="Associate Software Engineer" company="Optum" tenure="August 2018 - October 2018"/>
+            <Job title="Applications Developer II" company="Optum" tenure="January 2018 - August 2018"/>
+            <Job title="Tech Dev Program Senior Associate" company="Optum" tenure="November 2016 - January 2018"/>
+            <Job title="Tech Dev Program Intern" company="Optum" tenure="June 2016 - August 2016"/>
+            <Job title="Software Engineering Intern" company="Maverick Software Consulting" tenure="March 2014 – April 2016"/>
+            <Job title="Software Quality Assurance Intern" company="Symantec" tenure="May 2015 – August 2015"/>
+            <Job title="Manager" company="Marnanteli's Pizza" tenure="January 2012 – February 2016"/>
+            <Job title="Fulfillment Associate" company="Merrill Corporation" tenure="July 2012 – February 2014"/>
+            <div className="spacer"></div>
         </div>
     );
 }
