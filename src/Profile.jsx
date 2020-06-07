46d8e40d657909edf53eae1e22167657ae706a3b
Brian Hollingsworth <bhollin42@gmail.com>
2020-06-06 20:47:10 -0500

    Updated profile links.

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -8,11 +8,15 @@ export default function Profile(props) {
             <span id="profile-span">
             <p id="foreward">I am a software engineer, and this is my ever-changing alternative to LinkedIn and
                 paper resume. Originally developed as a <a
-                id="original-repo-link"
+                className="profile-link"
                 href="https://github.com/brianHollingsworth/brian.hollingsworth.codes"
                 target="_blank"
                 rel="noopener noreferrer">Node.js</a> application on the free tier of OpenShift, it has
-                been refactored into React and placed on AWS.</p>
+                been refactored into React and placed on <a
+                className="profile-link"
+                href="https://aws.amazon.com/amplify/"
+                target="_blank"
+                rel="noopener noreferrer">AWS Amplify</a>.</p>
             </span>
         </div>
     );
