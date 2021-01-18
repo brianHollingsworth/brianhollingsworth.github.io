a40aebb992f99025fa5fd8511aa57db3ab96f212
Brian Hollingsworth <bhollin42@gmail.com>
2021-01-17 21:52:29 -0600

    Patched "operation is insecure" error in Firefox.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -127,9 +127,11 @@ function checkValidServiceWorker(swUrl, config) {
 }
 
 export function unregister() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(registration => {
-      registration.unregister();
-    });
+  if (!isLocalhost) {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then(registration => {
+        registration.unregister();
+      });
+    }
   }
 }
