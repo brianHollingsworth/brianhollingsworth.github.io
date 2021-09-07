bf03ab69edc13323be85a368947cbc2c5f0422c0
Brian Hollingsworth <bhollin42@gmail.com>
2021-09-07 09:51:11 -0500

    Clean up Credly URLs.

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -19,11 +19,11 @@ export default function Profile(props) {
                 target="_blank"
                 rel="noopener noreferrer">AWS Amplify</a>.<br/><br/>My fandom of AWS drove me to become an <a
                 className="aws-certified-cloud-practitioner"
-                href="https://www.youracclaim.com/badges/5931d596-c2da-4b98-ace2-81e46d0c231b/public_url"
+                href="https://www.credly.com/badges/f37e7393-999b-4d45-92ac-49a29a9468de"
                 target="_blank"
                 rel="noopener noreferrer">AWS Certified Cloud Practitioner</a> and an <a
                 className="aws-certified-developer-associate"
-                href="https://www.credly.com/badges/07c5983e-d7f0-43d2-84a4-6dac97b6be3c?source=linked_in_profile"
+                href="https://www.credly.com/badges/07c5983e-d7f0-43d2-84a4-6dac97b6be3c"
                 target="_blank"
                 rel="noopener noreferrer">AWS Certified Developer – Associate</a>. A professional-level certification will be my next pursuit.
             </p>
